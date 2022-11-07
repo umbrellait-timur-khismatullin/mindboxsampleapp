ab92ce611c315473f37ef4526e96376c268ae0bb
timur.khismatullin <timur.khismatullin@umbrellait.com>
2022-11-08 00:17:26 +0300

    move plugin to activity

--- android/app/src/main/kotlin/com/example/mindboxsampleapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mindboxsampleapp/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.mindboxsampleapp
-
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import cloud.mindbox.mindbox_android.MindboxAndroidPlugin
+import android.os.Handler
+import android.os.Looper
 import cloud.mindbox.mobile_sdk.Mindbox
-
+import cloud.mindbox.mobile_sdk.MindboxConfiguration
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 
 class MainActivity : FlutterActivity() {
+    lateinit  var channel: MethodChannel
+    private var deviceUuidSubscription: String? = null
+    private var tokenSubscription: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +35,89 @@ class MainActivity : FlutterActivity() {
                 // Передача ссылки из пуша во Flutter
                 val link = Mindbox.getUrlFromPushIntent(intent) ?: ""
                 val payload = Mindbox.getPayloadFromPushIntent(intent) ?: ""
-                MindboxAndroidPlugin.pushClicked(link, payload)
+                Handler(Looper.getMainLooper()).post {
+                    channel.invokeMethod("pushClicked", listOf(link, payload))
+                }
+            }
+
+        }
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        channel = MethodChannel(flutterEngine.dartExecutor, "mindbox.cloud/flutter-sdk")
+        channel.setMethodCallHandler { call, result ->
+            when (call.method) {
+                "getSdkVersion" -> {
+                    result.success(Mindbox.getSdkVersion())
+                }
+                "init" -> {
+                    if (call.arguments is HashMap<*, *>) {
+                        val args = call.arguments as HashMap<*, *>
+                        val domain: String = args["domain"] as String
+                        val endpointId: String = args["endpointAndroid"] as String
+                        val previousDeviceUuid: String = args["previousDeviceUUID"] as String
+                        val previousInstallId: String = args["previousInstallationId"] as String
+                        val subscribeIfCreated: Boolean =
+                            args["subscribeCustomerIfCreated"] as Boolean
+                        val shouldCreateCustomer: Boolean = args["shouldCreateCustomer"] as Boolean
+                        val config = MindboxConfiguration.Builder(context, domain, endpointId)
+                            .setPreviousDeviceUuid(previousDeviceUuid)
+                            .setPreviousInstallationId(previousInstallId)
+                            .subscribeCustomerIfCreated(subscribeIfCreated)
+                            .shouldCreateCustomer(shouldCreateCustomer)
+                            .build()
+                        Mindbox.init(context, config, listOf())
+                        result.success("initialized")
+                    } else {
+                        result.error("-1", "Initialization error", "Wrong argument type")
+                    }
+                }
+                "getDeviceUUID" -> {
+                    if (deviceUuidSubscription != null) {
+                        Mindbox.disposeDeviceUuidSubscription(deviceUuidSubscription!!)
+                    }
+                    deviceUuidSubscription = Mindbox.subscribeDeviceUuid { uuid ->
+                        result.success(uuid)
+                    }
+                }
+                "getToken" -> {
+                    if (tokenSubscription != null) {
+                        Mindbox.disposePushTokenSubscription(tokenSubscription!!)
+                    }
+                    tokenSubscription = Mindbox.subscribePushToken { token ->
+                        result.success(token)
+                    }
+                }
+                "executeAsyncOperation" -> {
+                    if (call.arguments is List<*>) {
+                        val args = call.arguments as List<*>
+                        Mindbox.executeAsyncOperation(context, args[0] as String, args[1] as String)
+                        result.success("executed")
+                    }
+                }
+                "executeSyncOperation" -> {
+                    if (call.arguments is List<*>) {
+                        val args = call.arguments as List<*>
+                        Mindbox.executeSyncOperation(
+                            context,
+                            args[0] as String,
+                            args[1] as String,
+                            { response ->
+                                result.success(response)
+                            },
+                            { error ->
+                                result.error(
+                                    error.statusCode.toString(),
+                                    error.toJson(),
+                                    null
+                                )
+                            })
+                    }
+                }
+                else -> {
+                    result.notImplemented()
+                }
             }
         }
     }
